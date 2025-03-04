905e3998d3b34554e3914778066d93b09074be79
Gustavo <grodriguez@contextglobal.com>
2025-03-03 23:36:37 -0500

    second commit golang

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "7845"
+	}
+	app := fiber.New()
+	app.Use((cors.New()))
+	app.Static("/", "./customer_frontend_app/dist")
 
-	fmt.Println("Hello World PIA")
+	fmt.Println("Starting server on port " + port)
+	app.Listen(":" + port)
+
 }
